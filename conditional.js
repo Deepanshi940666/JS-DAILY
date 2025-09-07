f4c832f0b65065a850fb7329f8e1b2cc85d4251a
Deepanshi Gupta <deepanshi102938@gmail.com>
2025-09-08 00:43:00 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,113 @@
+//coditional statement
+
+//if-else statement 
+
+/*let mode= "blue";
+let color;
+if (mode==="dark")
+{   
+  color= " black";
+} 
+else 
+{ 
+   color= " white";
+}
+ console.log(color);
+//extra example
+let aage= 18;
+let validity;
+if (aage>=18) 
+{
+    console.log("you can vote");
+}
+else 
+{
+     console.log("you cannot vote");
+}
+
+
+// u can aklso use if statements only for conditions
+let age= 13;
+if (age>=18) 
+{
+    console.log("you can vote");
+}
+if (age<18)
+{
+     console.log("you cannot vote");
+}
+49
+// odd or even no.
+let number = 95;
+if (number%2===0)
+{
+    console.log(number," is even");
+}
+else 
+{
+    console.log(number," is odd");   // 95 is odd
+}
+
+// check the age range
+let agee = 38;
+if (agee<18 && agee >= 0)
+    console.log("Child");
+else if (agee<40)
+     console.log("Adult");
+else
+     console.log("Old age");
+
+// switch case
+console.log("Enter a no. \n 1.Maths \n 2. English \n 3. Hindi\n");
+let subject = 3;
+
+switch(subject)
+{
+    case 1:
+        console.log("MATHS");
+        break;
+    case 2:
+        console.log("ENGLISH");
+        break;
+    case 3:
+        console.log("HINDI");
+        break;
+    default:
+         console.log("NOT VALID SUBJECT");
+         break;
+}
+
+// taking input form user
+
+let section = prompt("Enter a section:");
+console.log(section);*/
+
+
+// check the no. is multiple of 5 or not
+
+let n = prompt("Enter a no.");
+console.log(n);
+if (n%5===0)
+    console.log(n ,"Multiple of 5");
+else 
+    console.log(n ," Not Multiple of 5");
+
+// grades acc to marks
+let mark = prompt("Enter a marks:");
+console.log(mark);
+if (mark<=100 && mark>=80)
+    console.log("A"); 
+else if(mark>=70)
+    console.log("B"); 
+else if(mark>=60)
+    console.log("C"); 
+else 
+    console.log("F"); 
+
+
+
+
+
+
+
+
---
+++ first.js
@@ -0,0 +1,93 @@
+// /*const student = {
+//     fullName:"riya yadav",
+//     age: "10",
+//     percentage:"89%",
+//     isPass: true
+// };
+// student.age = student.age + 1;
+// console.log(student.age)
+// const profile = {
+//     fullName: "shradhakhapra",
+//     isfollow: true,
+//     posts: 195,
+//     followers: "569k",
+//     description: "apnacollege"
+// };*/
+ 
+
+
+// // let a = 2 ;
+// // // let c=a+b;
+// // // console.log("sum=",c);
+// // // console.log("modulus=",a%b);
+// // //a=a++;
+// // console.log("a=", a);
+// // //a = a++;
+// // console.log("a--=",a--);
+// // console.log("a=", a);
+
+
+// //assignment operator
+// let a=20;
+// console.log("a+4=", a+=4);
+// a=20;
+// console.log("a-4=", a-=4);
+// a=20;
+// console.log("a*4=", a*=4);
+// a=20;
+// console.log("a/4=", a/=4);
+// a=20;
+// console.log("a%4=", a%=4);
+// a=20;
+// console.log("a**4=", a**4);
+
+
+
+
+// //comparison operator
+//  let a=20 ,b=30;
+// console.log("a=b",a==b);
+// console.log("a!=b",a!=b);
+
+//  a=3, b="3";
+// console.log("a=b",a==b);//true bcz string b is converted into number and a is also same no.
+
+
+// console.log("a=b",a===b);
+// console.log("a!=b",a!==b);
+
+// a=3, b=4;
+// console.log("a>b",a>b);
+// console.log("a<b",a<b);
+// console.log("a>=b",a>=b);
+// console.log("a<=b",a<=b);
+
+
+// // logical operators
+// // && use
+ 
+// let a=20;
+// let b=30;
+//  console.log("a<b and a=20 is true or false:",a<b && a===20);
+
+// // || use
+// console.log("a<b or a=20 is:",a<b || a===20);
+
+// // ! use
+// console.log("!(a>b):",!(a>b));
+
+
+
+
+// //unary operator
+
+// //in the console
+
+
+// //ternary operator
+
+// let a=20;
+// let b=30;
+
+// a>b ? console.log("true") :  console.log("false") ;
+ 
